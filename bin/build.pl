c6e3cf368426b455d1b0d4e85792b0323cbd3ba0
Leon Brocard <acme@astray.com>
2009-07-09 14:53:45 +0100

    build cassandra

--- bin/build.pl
+++ bin/build.pl
@@ -11,8 +11,11 @@ my $thrift_trunk_configure = file( $thrift_trunk_dir, 'configure' );
 my $thrift_trunk_makefile  = file( $thrift_trunk_dir, 'Makefile' );
 my $thrift_trunk_thrift
     = file( $thrift_trunk_dir, 'compiler', 'cpp', 'thrift' );
-my $cassandra_trunk_dir  = dir( cwd, 'cassandra-trunk' );
 my $thrift_installed_dir = dir( cwd, 'thrift' );
+my $cassandra_trunk_dir  = dir( cwd, 'cassandra-trunk' );
+my $cassandra_class
+    = dir( cwd, 'cassandra-trunk', 'build', 'classes', 'org', 'apache',
+    'cassandra', 'service', 'CassandraDaemon.class' );
 
 unless ( -d $thrift_trunk_dir ) {
     say 'Fetching Thrift';
@@ -50,3 +53,16 @@ unless ( -d $thrift_installed_dir ) {
     die 'Failed to make' unless -d $thrift_installed_dir;
 }
 
+unless ( -d $cassandra_trunk_dir ) {
+    say 'Fetching Cassandra';
+    system
+        "svn checkout http://svn.apache.org/repos/asf/incubator/cassandra/trunk $cassandra_trunk_dir";
+    die 'Failed to fetch' unless -d $cassandra_trunk_dir;
+}
+
+unless ( -f $cassandra_class ) {
+    say 'Building Cassandra';
+    chdir $cassandra_trunk_dir;
+    system "ant";
+    die 'Failed to build' unless -f $cassandra_class;
+}
