5fee6e8ca32c9df2cc9a0d5ac9c10ad333c7833e
Leon Brocard <acme@astray.com>
2009-07-10 15:06:28 +0100

    add more examples

--- play.pl
+++ play.pl
@@ -3,14 +3,35 @@ use strict;
 use warnings;
 use lib 'lib';
 use Net::Cassandra;
+use Perl6::Say;
 
 my $cassandra = Net::Cassandra->new;
 my $client    = $cassandra->client;
 
-my $key_user_id = '3141';
-my $timestamp   = time;
+my $key       = '123';
+my $timestamp = time;
+
 eval {
-    $client->insert( 'Table1', $key_user_id, 'Standard1:name', 'Leon Brocard',
-        $timestamp );
+    $client->insert( 'Table1', $key, 'Standard1:name', 'Leon Brocard',
+        $timestamp, 0 );
 };
 die $@->why if $@;
+
+eval {
+
+    $client->remove( 'Table1', $key, 'Standard1:age', $timestamp );
+};
+die $@->why if $@;
+
+my $column;
+eval { $column = $client->get_column( 'Table1', $key, 'Standard1:name' ); };
+die $@->why if $@;
+say $column->{columnName}, ', ', $column->{value}, ', ', $column->{timestamp};
+
+my $slice;
+eval { $slice = $client->get_slice( 'Table1', $key, 'Standard1', -1, -1 ); };
+die $@->why if $@;
+
+foreach my $row (@$slice) {
+    say $row->{columnName}, ', ', $row->{timestamp}, ', ', $row->{value};
+}
